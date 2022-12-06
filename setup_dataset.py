25b5e3ff761e2548bf2ad49bf97feae208f78fb2
justindeamattson <72591585+justindeamattson@users.noreply.github.com>
2022-12-06 15:56:52 -0800

    add2

--- setup_dataset.py
+++ setup_dataset.py
@@ -11,16 +11,38 @@ def main():
     # These csvs will be used as the data loaded in any of the experiments to ensure consistency in data splits
     ###############################################################################################
     # DATASET CONFIGURATIONS
-    columns = ['team', 'skill', 'skill_type', 'skill_subtype', 'home_team_score', 'visiting_team_score', 'home_setter_position', 'visiting_setter_position', 'serving_team', 'home_touch', 'away_touch', 'point_won_by']
+    columns = ['team', 'skill', 'skill_type', 'skill_subtype', 'home_team_score', 'visiting_team_score', 'home_setter_position', 'visiting_setter_position', 'serving_team', "start_coordinate_x",	"start_coordinate_y",	"mid_coordinate_x",	"mid_coordinate_y",	"end_coordinate_x",	"end_coordinate_y",'home_touch', 'away_touch', 'point_won_by']
     data_path = 'export_our_test_value_FINAL.csv'
     ###############################################################################################
     # dataset = VolleyballDataset(data_path=data_path)
     print("Started processing data")
     dataset = pd.read_csv(data_path)
+    #ataset.replace(to_replace= "nan", value=0)
+    #print(dataset)
     print("Loaded data")
+
+    #for i, row in dataset.iterrows():
+        #print (dataset._get_value(i, 'mid_coordinate_x', takeable=False))
+        #if dataset._get_value(i, 'mid_coordinate_x', takeable=False) is True:
+          #  print("hi")
+            #df.at[row,'team']=1
+          #  dataset._set_value(i, 'mid_coordinate_x', 0)
+    
+    dataset = dataset.fillna(0)
+
+    #dataset['mid_coordinate_x Column'] = dataset['mid_coordinate_x'].fillna(100)
 
     # Only use columns above
     dataset = dataset[columns]
+
+    #if [dataset['start_coordinate_x'] == ""]:
+     #   dataset['start_coordinate_x'] = 0
+
+    #if [dataset['mid_coordinate_x'] == ""]:
+    #    dataset['mid_coordinate_x'] = 0
+
+
+
 
     # point_won_by column should be 0 and 1
     dataset['point_won_by'] -= 1
